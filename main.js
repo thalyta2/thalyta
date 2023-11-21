8b77123daebd7e9ce9306498a7e60f8a2428c2d8
thalyta2 <125575299+thalyta2@users.noreply.github.com>
2023-11-21 08:38:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function tocaSomPom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+   
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSomPom(idAudio);
+    }  
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+        
+        if (evento.code == 'Space') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
